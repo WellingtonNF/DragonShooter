6c0724374a0d3308d89d3a51ed4eddbf6e45987b
Wellington Pedroso <eto.surf307@gmail.com>
2025-03-02 13:17:20 -0300

    Repair const file MENU

--- code/Const.py
+++ code/Const.py
@@ -5,7 +5,7 @@ COLOR_WHITE = (255, 255, 255)
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
-               'NEW GAME 2P - COMPETITIVE'
+               'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
 
--- code/Menu.py
+++ code/Menu.py
@@ -21,7 +21,7 @@ class Menu:
             self.menu_text(50, 'Shooter', COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 30 * i))
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
 
             pygame.display.flip()  # atualiza a imagem na surface, ou seja, ele vai pegar a imagem do destino e colocar no retangulo
 
