0570d56978596634553cd4fab941d12a12214c80
Wellington Pedroso <eto.surf307@gmail.com>
2025-03-09 12:23:21 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/Const.py
+++ code/Const.py
@@ -1,13 +1,22 @@
 # C
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 0)
+
+# E
+ENTITY_SPEED = {
+    'level1bg0': 0,
+    'level1bg1': 1,
+    'level1bg2': 1,
+    'level1bg3': 3,
+}
 
 # M
-MENU_OPTION = ('NEW GAME 1P',
-               'NEW GAME 2P - COOPERATIVE',
-               'NEW GAME 2P - COMPETITIVE',
-               'SCORE',
-               'EXIT')
+MENU_OPTION = ('NEW GAME 1P',  # 0
+               'NEW GAME 2P - COOPERATIVE',  # 1
+               'NEW GAME 2P - COMPETITIVE',  # 2
+               'SCORE',  # 3
+               'EXIT')  # 4
 
 # W
 WIN_WIDTH = 576
--- code/Menu.py
+++ code/Menu.py
@@ -2,7 +2,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -11,17 +11,22 @@ class Menu:
         self.surf = pygame.image.load('./asset/menubg.png')  # load img
         self.rect = self.surf.get_rect(left=0, top=0)  # define um retangulo invisivel no ponto 0 da janela
 
-    def run(self):
+    def run(self, ):
+        menu_option = 0
         pygame.mixer.music.load('./asset/Menu.wav')  # load music menu
         pygame.mixer.music.play(-1)  # tocar a musica carregada, o -1 significa colocar ela em loop, tocar infinito
 
         while True:
+            # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)  # primeiro desenha o fundo / o source é a origem, o dest é o destino da imagem
-            self.menu_text(50, 'Dragon', COLOR_WHITE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, 'Shooter', COLOR_WHITE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, 'Dragon', COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, 'Shooter', COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
 
             pygame.display.flip()  # atualiza a imagem na surface, ou seja, ele vai pegar a imagem do destino e colocar no retangulo
 
@@ -30,6 +35,19 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close Window
                     quit()  # End pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:  # DOWN KEY
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:  # UP KEY
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = 4
+                    if event.key == pygame.K_RETURN:  # ENTER
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
